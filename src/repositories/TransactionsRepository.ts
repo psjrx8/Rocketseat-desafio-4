8dd1c047f6a9fee67c5e9bf2a429c75fef8408cf
Paulo Junior <psjuniorx8@gmail.com>
2020-07-25 15:44:11 -0300

    Entrega do desafio 04

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,55 @@ interface Balance {
   total: number;
 }
 
+interface createTransactionDTO {
+  title:string,
+  value:number,
+  type: 'income' | 'outcome';
+}
+
+interface updateBalanceDTO {
+  value: number,
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
+  private balance: Balance = {
+    income: 0,
+    outcome: 0,
+    total: 0
+  };
 
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    const transactions = this.transactions;
+
+    return transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.balance;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public updateBalance({value, type}:updateBalanceDTO): void {
+    if(type === 'income') {
+      this.balance.income += value;
+    } else if (type === 'outcome') {
+      this.balance.outcome += value;
+    }
+    this.balance.total = this.balance.income - this.balance.outcome;
+  }
+
+  public create({ title, value, type } : createTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface createTransactionDTO {
+  title:string,
+  value:number,
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,23 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type } : createTransactionDTO ): Transaction {
+
+    const balance = this.transactionsRepository.getBalance();
+
+    if(type === 'outcome' && value > balance.total) {
+      throw Error ('There is not enough balance for this operation.')
+    };
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type
+    });
+
+    this.transactionsRepository.updateBalance({value, type});
+
+    return transaction;
   }
 }
 
